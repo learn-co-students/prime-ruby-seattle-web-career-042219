19c4028860499c1b85da3e013951acd2375672f2
Alex Kitelinger <bureaucratix@gmail.com>
2019-03-23 14:12:22 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,27 @@
-# Add  code here!
+def prime?(num)
+  #primearr = []
+
+  if num <=1
+    return false
+  elsif num == 2
+    return true
+  else
+      sqnum = Math.sqrt(num)
+      (2..sqnum).each do |i|
+        if num % i == 0
+          #primearr.push(1)
+          return false
+        end
+      end
+      return true
+  end
+  # if primearr.include?(1)
+  #   return false
+  # else
+  #   return true
+  # end
+end
+
+puts prime?(-1)
+puts prime?(17)
+puts prime?(400000)
