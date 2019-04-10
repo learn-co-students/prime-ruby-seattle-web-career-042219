20813e67fdd97c14afd0e8eacac6327ac840631f
Sean Watters <sean@h2os.org>
2019-04-10 08:53:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(number)
+
+  answer = true
+
+  if number > 1
+    for i in 2..(number - 1)
+      if (number % i) == 0
+        answer = false
+      end
+    end
+
+  else
+    answer = false
+  end
+
+  return answer
+end
