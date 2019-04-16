86eac99e358026152755b60e0bf0ef2ad5089f1f
Greg Morgan <gregory.morgan@flatironschool.com>
2019-04-15 17:27:19 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(num)
+    ans=true
+    if num <2
+        ans=false
+    end
+    i=2
+    while i<num
+        if num%i == 0
+            ans=false
+            break
+        end
+        i+=1
+    end
+    ans
+end
